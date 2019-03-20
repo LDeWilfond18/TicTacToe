b5e82d97fcbb33f0127a0cc255f1070704dfed49
Logan <Logan.dewilfond@gmail.com>
2019-03-20 09:33:56 -0500

    Started

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.slot1 = new System.Windows.Forms.Button();
+            this.slot2 = new System.Windows.Forms.Button();
+            this.slot3 = new System.Windows.Forms.Button();
+            this.slot4 = new System.Windows.Forms.Button();
+            this.slot5 = new System.Windows.Forms.Button();
+            this.slot6 = new System.Windows.Forms.Button();
+            this.slot7 = new System.Windows.Forms.Button();
+            this.slot8 = new System.Windows.Forms.Button();
+            this.slot9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // slot1
+            // 
+            this.slot1.Location = new System.Drawing.Point(63, 82);
+            this.slot1.Name = "slot1";
+            this.slot1.Size = new System.Drawing.Size(166, 156);
+            this.slot1.TabIndex = 0;
+            this.slot1.UseVisualStyleBackColor = true;
+            // 
+            // slot2
+            // 
+            this.slot2.Location = new System.Drawing.Point(235, 82);
+            this.slot2.Name = "slot2";
+            this.slot2.Size = new System.Drawing.Size(166, 156);
+            this.slot2.TabIndex = 1;
+            this.slot2.UseVisualStyleBackColor = true;
+            // 
+            // slot3
+            // 
+            this.slot3.Location = new System.Drawing.Point(407, 81);
+            this.slot3.Name = "slot3";
+            this.slot3.Size = new System.Drawing.Size(163, 157);
+            this.slot3.TabIndex = 2;
+            this.slot3.UseVisualStyleBackColor = true;
+            // 
+            // slot4
+            // 
+            this.slot4.Location = new System.Drawing.Point(63, 244);
+            this.slot4.Name = "slot4";
+            this.slot4.Size = new System.Drawing.Size(166, 157);
+            this.slot4.TabIndex = 3;
+            this.slot4.UseVisualStyleBackColor = true;
+            // 
+            // slot5
+            // 
+            this.slot5.Location = new System.Drawing.Point(235, 244);
+            this.slot5.Name = "slot5";
+            this.slot5.Size = new System.Drawing.Size(166, 157);
+            this.slot5.TabIndex = 4;
+            this.slot5.UseVisualStyleBackColor = true;
+            // 
+            // slot6
+            // 
+            this.slot6.Location = new System.Drawing.Point(406, 244);
+            this.slot6.Name = "slot6";
+            this.slot6.Size = new System.Drawing.Size(162, 157);
+            this.slot6.TabIndex = 5;
+            this.slot6.UseVisualStyleBackColor = true;
+            // 
+            // slot7
+            // 
+            this.slot7.Location = new System.Drawing.Point(63, 407);
+            this.slot7.Name = "slot7";
+            this.slot7.Size = new System.Drawing.Size(166, 158);
+            this.slot7.TabIndex = 6;
+            this.slot7.UseVisualStyleBackColor = true;
+            // 
+            // slot8
+            // 
+            this.slot8.Location = new System.Drawing.Point(235, 407);
+            this.slot8.Name = "slot8";
+            this.slot8.Size = new System.Drawing.Size(165, 158);
+            this.slot8.TabIndex = 7;
+            this.slot8.UseVisualStyleBackColor = true;
+            // 
+            // slot9
+            // 
+            this.slot9.Location = new System.Drawing.Point(406, 407);
+            this.slot9.Name = "slot9";
+            this.slot9.Size = new System.Drawing.Size(162, 158);
+            this.slot9.TabIndex = 8;
+            this.slot9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(87, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Welcome to TicTacToe, player is X\'s click one of the slots to begin.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 594);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.slot9);
+            this.Controls.Add(this.slot8);
+            this.Controls.Add(this.slot7);
+            this.Controls.Add(this.slot6);
+            this.Controls.Add(this.slot5);
+            this.Controls.Add(this.slot4);
+            this.Controls.Add(this.slot3);
+            this.Controls.Add(this.slot2);
+            this.Controls.Add(this.slot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button slot1;
+        private System.Windows.Forms.Button slot2;
+        private System.Windows.Forms.Button slot3;
+        private System.Windows.Forms.Button slot4;
+        private System.Windows.Forms.Button slot5;
+        private System.Windows.Forms.Button slot6;
+        private System.Windows.Forms.Button slot7;
+        private System.Windows.Forms.Button slot8;
+        private System.Windows.Forms.Button slot9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
